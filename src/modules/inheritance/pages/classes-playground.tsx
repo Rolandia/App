842b0c482ab971f84037ccbc952e4603683682fd
Mauro Vieira <mauroccvieira@gmail.com>
2021-10-18 22:51:14 -0300

    no clique do botão

--- src/modules/inheritance/pages/classes-playground.tsx
+++ src/modules/inheritance/pages/classes-playground.tsx
@@ -1,73 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Store } from "../classes/Store";
 import { Address } from "../classes/Address";
+import { setInterval } from "timers";
 
-export class ClassesPlayground extends Component {
-  static propTypes = {};
-
-  render() {
-    const rolanches = new Store(
-      1,
-      "Rolanches",
-      "RolanchesDaSilva",
-      123123123,
-      new Address(
-        "Rua lindolfo pinheiro",
-        "00",
-        "rosário",
-        "Minas Gerais",
-        "36700-000"
-      )
-    );
-    const digao = new Store(
-      2,
-      "digao",
-      "digaoasdasd",
-      123123123,
-      new Address(
-        "Rua lindolfo pinheiro",
-        "00",
-        "rosário",
-        "Minas Gerais",
-        "36700-000"
-      )
-    );
-    const vilelao = new Store(
-      3,
-      "vilelao",
-      "vilelaoNome",
-      123123123,
-      new Address(
-        "Rua lindolfo pinheiro",
-        "00",
-        "rosário",
-        "Minas Gerais",
-        "36700-000"
-      )
-    );
-    const stores: Store[] = [rolanches, vilelao, digao];
-    return (
-      <div>
-        <ul>
-          {stores.map((st) => {
-            return (
-              <li
-                key={st.id}
-                style={{
-                  color: "blue",
-                  fontWeight: 900,
-                  fontFamily: "Comic Sans",
-                }}
-              >
-                {st.businessName}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+interface State {
+  startAt?: number;
+  endAt?: number;
+  tempo: number;
 }
+export const ClassesPlayground = () => {
+  const [currentCount, setCount] = useState(10);
+  const onClickDoBotao = () => {
+    console.log("Botão clicado");
+  };
+  return (
+    <div>
+      <ul>
+        <div>Tempo: {currentCount} </div>
+        <button onClick={onClickDoBotao}>Iniciar</button>
+      </ul>
+    </div>
+  );
+};
 
 export default ClassesPlayground;
